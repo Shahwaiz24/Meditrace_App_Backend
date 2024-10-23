27d990dad8b16d5c973d004b426aefd0dcf92e0c
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-10-23 11:51:20 -0700

    Updated

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,12 +1,17 @@
+import * as admin from 'firebase-admin';
+import path from 'path';
 
 export default class FirebaseConfig {
-
-    static async initializeFirebaseApp(){
-      const serviceAccount = require('C:/Users/Leo/Desktop/Flutter_Projects/Meditrace_App_Backend/Firebase-Service-File/meditrace-app-firestore-firebase-adminsdk-c3odt-aac25b3ea5.json');
-      const admin = require('firebase-admin');
-        admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount),
-          storageBucket: 'gs://meditrace-app-firestore.appspot.com'
-        });
+  static async initializeFirebaseApp() {
+    if (!admin.apps.length) { // Check if Firebase is already initialized
+      const serviceAccount = require(path.resolve(__dirname, '../Firebase-Service-File/meditrace-app-firestore-firebase-adminsdk-c3odt-aac25b3ea5.json'));
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        storageBucket: 'gs://meditrace-app-firestore.appspot.com',
+      });
+      console.log("Firebase initialized");
+    } else {
+      console.log("Firebase already initialized");
     }
+  }
 }
