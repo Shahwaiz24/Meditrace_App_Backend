d2e7c191f9bfcfe31cd0c61f992e408910fdc861
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-10-24 06:57:08 -0700

    Finally Its Completed Without any Errro

--- dist/config/firebase-config.js
+++ dist/config/firebase-config.js
@@ -24,14 +24,17 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const admin = __importStar(require("firebase-admin"));
-const path = __importStar(require("path"));
+let firebaseApp;
 class FirebaseConfig {
-    static initializeFirebaseApp() {
+    static async initializeFirebaseApp() {
         try {
             if (!admin.apps.length) {
-                const serviceAccount = require("./serviceAccountKey.json");
-                admin.initializeApp({
-                    credential: admin.credential.cert(path.resolve("./serviceAccountKey.json")),
+                firebaseApp = await admin.initializeApp({
+                    credential: admin.credential.cert({
+                        clientEmail: "firebase-adminsdk-c3odt@meditrace-app-firestore.iam.gserviceaccount.com",
+                        projectId: "meditrace-app-firestore",
+                        privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDaijXvwg94Pskx\n/K7/s5rP7mbi7pqXFYFopaLSoIOXJCilrHqgb5zomUFYX5mEB4mxUFOa0hUDq2eA\n8hhG5busc0vQ4bPnNFe4Ow1f4yk7J5/dFyL8R+oVd6KLOtfh2A7WxvgEEPJyMMlJ\nEYfsBxb1dKOM7VIKxQWxgVnaq83nnt6gGIOdu/C9obgueZ1ehm6Gd2WWNw86Q6T9\n/oReQaqu1MpMqYIE7S5inxI266ETs8wYVM8qi+1DtiPVLF2Rcufc4uUSDAESexF+\nybsYud37a/HY4CT3Z9dmejlb+R98rHenkEyVIzmck9kfZE90yFCJABO1iiNz4FFm\niBoNEl4RAgMBAAECggEARAOkwrAuWVmNC3IdUrZmlZUYT0M8OI44XsXCFXRgT+8w\neVNjPMBP4LIlvKfQIt+nslgSFQqSALjdNyvhc1s3W/XsIB/Gekv3iv5R7MhJgkAU\nSF5VhJerGjBIFbXkD0VLZPu6n6VYl0JousVqfII+WnkJF6iakoB8A6ECwoQayp07\nORaLsm4CrV7JHL5Pf46B1vHgKKZNXdIfGFnjXOJGMohpfPH3J/pXiPxilHnNlmD8\nqnSIpc6N9pUqu1wk+pRXFYMO+9FsY/RvH1b7v4TrkQM/NvADyNb7IGhl6wnrwCOG\ns5TsDaFAPb3TYPU5Kgo9VhKGOzEzFnxPGR3DYrGNywKBgQD4up4PxVMkGAbv+Ibd\njQl4J8gcjPLF2M1p0ih8TnZRiE685FLLF4QZmwAuVuNd4ukGuQc5VT9Hj/UNitlY\ncBvN5DLkj2/fVRGjDcFm8u9xyAcmJ2/buoCO2yCSg4kUHujh2OYeXMYvwEPorQ0S\nPBQB3tcF0SvAPXrDbuGZB/g34wKBgQDg7au+YXwB9AZaJJJ1vjJo4yp5SFfY8h1A\nKrzwYwNej/1h5Czq+lHCzDPtn3OhdDJ8rtGKbrW46Pabi7lGxr3rKee3efnRQcwr\npz75tC6BL+nCjBouqAY9w6eKTHGkG2VtpGWrFB4768l6He00jtGyiWdPgCecV9WV\n1OXf9SGsewKBgQDggQE+VuuOfql0XKzOuQi9DZtQOeC/t8rhIzIr0V52hUQctzIL\n4nP38LIfIYokKjYPPV9J3qo5W7Tivlsjuj0+gCV3rAhcWxqKRFY3KDv8RYI+bAZJ\nHHQBfcROuq82HBg0EmTTLwYr/6zMDbboqJWegpcuxvhokpWGZUT3rfvb5QKBgEor\ntYWLCeRu/qU9CBrRhDf4KOpjg6xjcmfVcAZo8BR/sNNV9cXqk4mD4t/Jw8yKz5cz\n4p8ekMgP8joqogcHxFrGr8Vd8JYjd1sxxF99HhS1owTnRD78Y0p8uUOun465BgJY\nwrneYXXq875UHgNF0TppeurJXMZ+ZE0LLl5irB7/AoGALN2BYoRWK+nxj6tiXD1d\nkS6a1RFQf274kRbLNq7LSa2jGtgJHjNnKrqZaYCUVsUII7XQonH+Gsi8gxUOYQf7\n0tt1M1mIaVXI1/a2oTb5piBKFsRfh80WFK9SZzFAjwaVvvwYJoWPwDgXEg/gY/fb\nDkBev/A5pVWWTFqpgsUN7oQ=\n-----END PRIVATE KEY-----\n",
+                    }),
                     storageBucket: 'meditrace-app-firestore.appspot.com',
                 });
             }
@@ -44,7 +47,7 @@ class FirebaseConfig {
         }
     }
     static async getfirebaseBucket() {
-        return await admin.storage().bucket();
+        return await firebaseApp.storage().bucket();
     }
 }
 exports.default = FirebaseConfig;
--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -3,19 +3,20 @@ import * as path from 'path';
 import { initializeApp } from "firebase/app";
 import { NextFunction } from 'express';
 
-
+let firebaseApp;
 export default class FirebaseConfig {
+  
   static async initializeFirebaseApp() {
     try {
       
       if (!admin.apps.length) {
   
   
-      await admin.initializeApp({
+     firebaseApp =  await admin.initializeApp({
           credential: admin.credential.cert({
             clientEmail: "firebase-adminsdk-c3odt@meditrace-app-firestore.iam.gserviceaccount.com",
             projectId: "meditrace-app-firestore",
-            privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDSlj9FEZCbksrd\nC+GBvk7neoT5S2QYBULml9a942eoGP8bNmQGBzJpw5FQxZLfKwpZnbxWT8JBUxjZ\nzPakRKxHZuTHTed4LU2tcHB90iY4s3vETKljl3/p4BVsmv18/DoE69lWt2oCPV3W\n++kNfqlgT+HW2oFKcHDTcGOGeqmFe67f+A2+qK+0g2OBuQAF0lr3W/iGcL0q6Fcr\nqNxv4dsoSPrTuOADGz7cKftIdscjQkE97ZoAJj2CAbM4EOqEw522dUw8xFyyfRKV\nAEqTCaVBuH5riIeEbJdAFbJUAhTxeEsnQ8hrcy9QECZFDMyOjBxWWNQ8gofqcoYt\niWO7bX2VAgMBAAECggEAFnI4XTrRkJuVbvlIuKUZ0PeQu+fKy/qnll+h3oyiLFs5\nueNSxwe2Wmqjjxn4HTx/tmynOgX3XTInyHNd6MV/HX88rHfDz934VR/Shb7EHzAW\nZaaMOZbY5gtiDTj6Avv5jTO8b09Oh92VpogkX7FcCW2KU3Xn6RDSHd9xZFCnyBfk\n2UDK2Jig+L5B6aAjmGiodobOHHSv4WCG1BcEB5fXLMUqisRrnw4Jdns87y9izBO9\npZb39vUTexZ9uuzRm46XN5xNIRZs/9VHH/DUoUycRGwrK6LOtaDZ2BPnK9/yl1AQ\nxIC/+G7cAyMietnyzS9RY4uuEJpGvdOwMsQIy5xC8QKBgQDt9etjCHi0i7onCj1q\nE3nf31sIPFgBSI/8JREZdRPo4z03KYetwUqTEnCsJQcqROZ+bJCwYlPNdZkpwPmo\nvMEiVh0XyZFttaBPpRWHcNjDu+Lf+MwsfmSkFmoZ5xnaCW7xRsVHHLGcFh7xWF0C\nTCiZta+NK1aHqalOuF0uwBSKmQKBgQDijRac6wP3E+zjawCgNMcpFqyubLL4YvZf\nEi3YsHOn487dAkaCxa9NkSa9vi+b3LU6LzZpnCvgtvdSWcVsGF67SCzJjcQp7O3N\n1O2LetUA7aihg/FHrRXzPRpHkjplCpInFMoN+MT3jG4UCLl6SjoT5xObFs2VtyjU\nIHQuGurEXQKBgCGdfdUNm0+V9YQSVD6p5auUd92f+Kalw4FREytPeLKLd1hVi48m\nIQzJrBUFXg/Aj/cICyB/o2sA09QsKJiHoZjiJVX46Yxf/feCUJpDfLuZzSDqmp0m\ndb0gVXi3RtWgQ4XeA3xaN1TfYxug6fKrI4kjO1tr5dtgQkSWksNJ6xzxAoGAQmYn\nooYe+2OTonPSEyJk3N4Qbm7dZX9bxjGFzmSpASP7lTr9f4aBArLwL9x2B7CATTVZ\nNUrYcVwu0KwO+MeLvEhDGC+8tq0CZblqhd7SXxtIuU36gY9xR4E5GpD5HHQksd+d\n+MpYsFUwTh7gfb8TlpG7ZVEhoCe2h7WbV4f8FuECgYEAuCpXOt0WCoF0gauK81/W\n68pP7jH1uztdqX6zeYQb1iR3qtU9p6W+uWmS8mVoN2lT5sZVf140P/zCTao2WRmB\nBm4+1PqsUN8wuDJp+KsY8NWhGp53D4YrQMDsLJgUdlRqedPxfAvWPECFNkqVp9fX\n84cXk+n6a2cBwlZeFEpWEUQ=\n-----END PRIVATE KEY-----\n",
+            privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDaijXvwg94Pskx\n/K7/s5rP7mbi7pqXFYFopaLSoIOXJCilrHqgb5zomUFYX5mEB4mxUFOa0hUDq2eA\n8hhG5busc0vQ4bPnNFe4Ow1f4yk7J5/dFyL8R+oVd6KLOtfh2A7WxvgEEPJyMMlJ\nEYfsBxb1dKOM7VIKxQWxgVnaq83nnt6gGIOdu/C9obgueZ1ehm6Gd2WWNw86Q6T9\n/oReQaqu1MpMqYIE7S5inxI266ETs8wYVM8qi+1DtiPVLF2Rcufc4uUSDAESexF+\nybsYud37a/HY4CT3Z9dmejlb+R98rHenkEyVIzmck9kfZE90yFCJABO1iiNz4FFm\niBoNEl4RAgMBAAECggEARAOkwrAuWVmNC3IdUrZmlZUYT0M8OI44XsXCFXRgT+8w\neVNjPMBP4LIlvKfQIt+nslgSFQqSALjdNyvhc1s3W/XsIB/Gekv3iv5R7MhJgkAU\nSF5VhJerGjBIFbXkD0VLZPu6n6VYl0JousVqfII+WnkJF6iakoB8A6ECwoQayp07\nORaLsm4CrV7JHL5Pf46B1vHgKKZNXdIfGFnjXOJGMohpfPH3J/pXiPxilHnNlmD8\nqnSIpc6N9pUqu1wk+pRXFYMO+9FsY/RvH1b7v4TrkQM/NvADyNb7IGhl6wnrwCOG\ns5TsDaFAPb3TYPU5Kgo9VhKGOzEzFnxPGR3DYrGNywKBgQD4up4PxVMkGAbv+Ibd\njQl4J8gcjPLF2M1p0ih8TnZRiE685FLLF4QZmwAuVuNd4ukGuQc5VT9Hj/UNitlY\ncBvN5DLkj2/fVRGjDcFm8u9xyAcmJ2/buoCO2yCSg4kUHujh2OYeXMYvwEPorQ0S\nPBQB3tcF0SvAPXrDbuGZB/g34wKBgQDg7au+YXwB9AZaJJJ1vjJo4yp5SFfY8h1A\nKrzwYwNej/1h5Czq+lHCzDPtn3OhdDJ8rtGKbrW46Pabi7lGxr3rKee3efnRQcwr\npz75tC6BL+nCjBouqAY9w6eKTHGkG2VtpGWrFB4768l6He00jtGyiWdPgCecV9WV\n1OXf9SGsewKBgQDggQE+VuuOfql0XKzOuQi9DZtQOeC/t8rhIzIr0V52hUQctzIL\n4nP38LIfIYokKjYPPV9J3qo5W7Tivlsjuj0+gCV3rAhcWxqKRFY3KDv8RYI+bAZJ\nHHQBfcROuq82HBg0EmTTLwYr/6zMDbboqJWegpcuxvhokpWGZUT3rfvb5QKBgEor\ntYWLCeRu/qU9CBrRhDf4KOpjg6xjcmfVcAZo8BR/sNNV9cXqk4mD4t/Jw8yKz5cz\n4p8ekMgP8joqogcHxFrGr8Vd8JYjd1sxxF99HhS1owTnRD78Y0p8uUOun465BgJY\nwrneYXXq875UHgNF0TppeurJXMZ+ZE0LLl5irB7/AoGALN2BYoRWK+nxj6tiXD1d\nkS6a1RFQf274kRbLNq7LSa2jGtgJHjNnKrqZaYCUVsUII7XQonH+Gsi8gxUOYQf7\n0tt1M1mIaVXI1/a2oTb5piBKFsRfh80WFK9SZzFAjwaVvvwYJoWPwDgXEg/gY/fb\nDkBev/A5pVWWTFqpgsUN7oQ=\n-----END PRIVATE KEY-----\n",
           }),
           storageBucket: 'meditrace-app-firestore.appspot.com',
         });
@@ -31,7 +32,7 @@ export default class FirebaseConfig {
   }
   
   static async getfirebaseBucket() {
-    return await admin.storage().bucket();
+    return await firebaseApp!.storage().bucket();
   }
 }
  
